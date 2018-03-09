6c32165724d2bdc508bd1af4f914e98dcca449c7
Roman Maximov <maximoff.r@gmail.com>
2018-03-09 15:29:05 +0400

    Point to production

--- server/xo-api-executors/xo-api-urls.js
+++ server/xo-api-executors/xo-api-urls.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://api-qa.crossover.com/api/';
+const BASE_URL = 'https://api.crossover.com/api/';
 const API = {
   getApplication: (appId) => `${BASE_URL}hire/applications/${appId}?avatarType=ACCOUNT_MANAGER`,
   search: (pageSize, page, avatarType) => `${BASE_URL}hire/applications/search?avatarType=${avatarType}&pageSize=${pageSize}&page=${page}`,
